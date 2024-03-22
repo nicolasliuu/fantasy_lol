776ff6621d460b05292a9c9dd04c49a7bedf44ab
Nicolas Liu <nicolasliu18@gmail.com>
2024-03-22 13:57:56 -0400

    scoring, schema updates

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,12 +1,42 @@
 from pydantic import BaseModel
 from typing import List, Optional
+from typing import List, Optional
+from .models import AnalyzedMatch
 
 class Account(BaseModel):
-    puuid: Optional[str] = None  # Riot's unique identifier for a player
-    gameName: Optional[str] = None
-    tagLine: Optional[str] = None
+    puuid: str  # Riot's unique identifier for a player
+    gameName: str
+    tagLine: str
     matchHistory: Optional[List[str]] = None  # List of match IDs
+    analyzed_matchHistory: Optional[List[AnalyzedMatch]] = None  # List of match IDs that have been analyzed
     playerId: Optional[str] = None  # Your custom player ID
+    kills: Optional[List[int]] # Number of kills for current period (patch)
+    deaths: Optional[List[int]] # Number of deaths for current period (patch)
+    assists: Optional[List[int]] # Number of assists for current period (patch)
+    kda_ratio: Optional[List[float]] # KDA ratio for current period (patch)
+    wins: Optional[List[int]] # Number of wins for current period (patch)
+    losses: Optional[List[int]] # Number of losses for current period (patch)
+    win_rate: Optional[List[float]] # Win rate for current period (patch)
+    score: Optional[List[float]] # AI-computed Score for current period (patch)
+    score_std: Optional[List[float]] # Standard deviation of AI-computed Score for current period (patch)
+    opponent_delta: Optional[List[float]] # Difference in AI-computed Score between player and opponent for current period (patch)
+
+class AnalyzedMatch(BaseModel):
+    # Same as a match ID except we attach the player's performance in the match with opponent laner's performance
+    matchId: str
+    date: int # unix timestamp? 
+    playerId: str
+    opponentId: str
+    kills: int
+    deaths: int
+    assists: int
+    dragons: int
+    heralds: int
+    barons: int
+    win: bool
+    score: float    
+    opponentScore: float
+    scoreDelta: float
 
 class PerkStyleSelectionDto(BaseModel):
     perk: Optional[int] = None
@@ -37,6 +67,7 @@ class ParticipantDto(BaseModel):
     assists: Optional[int] = None
     win: Optional[bool] = None
     perks: Optional[PerksDto] = None
+    
     # Add more fields as necessary
 
 class TeamDto(BaseModel):
--- backend/db/scoring.py
+++ backend/db/scoring.py
@@ -1,2 +1,3 @@
 # We use stored match data to perform calculations on each player's performance and store the results in the database
 
+# Given an Account object: 
